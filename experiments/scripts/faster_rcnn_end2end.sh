41e578b505dfb017ea3d896428fb3ae4b17e6483
ElaineBao <elaine.bao@foxmail.com>
2017-03-17 11:06:08 +0800

    add imagenet train shell command

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -38,6 +38,12 @@ case $DATASET in
     PT_DIR="coco"
     ITERS=490000
     ;;
+  imagenet)
+    TRAIN_IMDB="imagenet_2015_train"
+    TEST_IMDB="imagenet_2015_val"
+    PT_DIR="imagenet"
+    ITERS=100000
+    ;;
   *)
     echo "No dataset given"
     exit
